59028927d5433a50f6c829dbc36290a79ca6266f
mohammadhasansojib <sajibhasan800080@gmail.com>
2026-06-25 23:42:06 +0600

    style: update catchAsync utility function

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, RequestHandler, Response } from "express";
-
+import { sendResponse } from "./sendResponse";
+import httpStatus from "http-status";
 
 
 export const catchAsync = (fn: RequestHandler) => {
@@ -11,14 +12,27 @@ export const catchAsync = (fn: RequestHandler) => {
         } catch (error: any) {
             console.log(error);
 
-            res.status(error.statusCode).json({
-                success: false,
-                data: {
-                    error,
-                },
-                message: error.message,
-                statusCode: error.statusCode,
-            })
+            const statusCode = error.statusCode || 500;
+
+            if (error.name === "JsonWebTokenError") {
+
+                sendResponse(res, {
+                    success: false,
+                    message: error.message,
+                    statusCode: httpStatus.UNAUTHORIZED,
+                    data: {error}
+                });
+
+            } else {
+
+                sendResponse(res, {
+                    success: false,
+                    message: error.message || "Internal server error",
+                    statusCode: statusCode,
+                    data: {error}
+                });
+
+            }
         }
     }
 }
