fbb890379252808c8e81d85749677c58ea6f9298
mohammadhasansojib <sajibhasan800080@gmail.com>
2026-06-26 12:16:23 +0600

    feat: add /me route and complete the endpoint

--- src/module/user/user.route.ts
+++ src/module/user/user.route.ts
@@ -1,10 +1,13 @@
 import express from "express"
 import userController from "./user.controller";
+import auth from "../../middleware/auth";
+import { Role } from "../../../generated/prisma/enums";
 
 const router = express.Router();
 
 
 router.post('/register', userController.register);
+router.get('/me', auth(Role.ADMIN, Role.AUTHOR, Role.USER), userController.getMyProfile);
 
 
 export default router;
