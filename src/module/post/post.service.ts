7a95a7d7e7504bdbeb1a8a198640c8819471f103
mohammadhasansojib <sajibhasan800080@gmail.com>
2026-06-29 21:17:01 +0600

    refactor: update getPostByIdFromDB service function

--- src/module/post/post.service.ts
+++ src/module/post/post.service.ts
@@ -1,5 +1,6 @@
 import { prisma } from "../../lib/prisma";
 import { AppError } from "../../utils/error";
+import httpStatus from "http-status"
 
 
 
@@ -24,6 +25,38 @@ const getAllPostsFromDB = async () => {
 }
 
 const getPostByIdFromDB = async (id: string) => {
+    const transactionPost = await prisma.$transaction(async (tx) => {
+        const post = await tx.post.findUnique({
+            where: {
+                id,
+            }
+        });
+        if (!post) {
+            throw new AppError("no post found", 404);
+        }
+
+        const updatedPost = await tx.post.update({
+            where: {
+                id,
+            },
+            data: {
+                views: {
+                    increment: 1,
+                }
+            }
+        });
+
+        return updatedPost;
+    })
+
+
+    if (!transactionPost) {
+        throw new AppError("no post found", httpStatus.NOT_FOUND);
+    }
+
+    return transactionPost;
+}
+
     const post = await prisma.post.findUnique({
         where: {
             id,
